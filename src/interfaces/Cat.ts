8bccf0a25a442f817fbdc9078f6a9dcefbcb76cb
Pavlo Leinonen <leinonenko@gmail.com>
2023-03-24 17:29:08 +0200

    getting started, interface + mongodb

--- src/interfaces/Cat.ts
+++ src/interfaces/Cat.ts
@@ -1,1 +1,24 @@
+import {Point} from 'geojson';
+import {Document, Types} from 'mongoose';
+import {User} from './User';
+
 // TODO: cat interface
+interface Cat extends Document {
+  cat_name: string;
+  owner: Types.ObjectId | User;
+  weight: number;
+  filename: string;
+  birthdate: Date;
+  location: Point;
+}
+
+interface CatTest {
+  _id?: string;
+  cat_name?: string;
+  owner?: User | string;
+  filename?: string;
+  weight?: number;
+  birthdate?: Date;
+  location?: Point;
+}
+export {Cat, CatTest};
--- src/interfaces/User.ts
+++ src/interfaces/User.ts
@@ -1,1 +1,21 @@
+import {Document} from 'mongoose';
+
 // TODO: user interface
+interface User extends Document {
+  user_name: string;
+  email: string;
+  password: string;
+  role: 'user' | 'admin';
+}
+
+interface UserOutput {
+  _id: string;
+  user_name: string;
+  email: string;
+}
+interface LoginUser {
+  user_name: string;
+  password: string;
+}
+
+export {User, UserOutput, LoginUser};
